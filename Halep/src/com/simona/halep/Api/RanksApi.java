d8c423a7d008bb10be9316c93f880e3666c6469d
rcijov <rcijov@gmail.com>
2015-11-07 11:13:50 -0500

    Working Ranks Api online - offline

--- Halep/src/com/simona/halep/Api/RanksApi.java
+++ Halep/src/com/simona/halep/Api/RanksApi.java
@@ -1,10 +1,16 @@
 package com.simona.halep.Api;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import android.app.Activity;
 
 import com.simona.halep.Database.Entities.Rank;
 import com.simona.halep.Database.Entities.Result;
@@ -14,6 +20,7 @@ public class RanksApi {
 
 	private static RanksApi instance = null;
 	private static HelperApi helpApi;
+	private static Activity activity = null;
 	
 	public static RanksApi getInstance()
 	{
@@ -21,12 +28,25 @@ public class RanksApi {
 		{
 			instance = new RanksApi();
 			helpApi = helpApi.getInstance();
+			activity = helpApi.getActivity();
 		}
 		return instance;
 	}
 	
 	public static ArrayList<Rank> getRanks()
 	{	
+		if(helpApi.isOnline())
+		{
+			return online();
+		}
+		else
+		{
+			return offline();
+		}
+	}
+	
+	private static ArrayList<Rank> online()
+	{
 		String urlString = "https://www.kimonolabs.com/api/9cvgr58m?apikey=EuXTaIb1UyOnvRL4HebQkXbTy1rfN6XY";
 		ArrayList<Rank> resultArray = new ArrayList<Rank>();
 		
@@ -59,4 +79,39 @@ public class RanksApi {
 		return resultArray;
 	}
 	
+	private static ArrayList<Rank> offline()
+	{
+		ArrayList<Rank> ranks = new ArrayList<Rank>();
+		String csvFile = "overviewRanks.csv";
+		BufferedReader br = null;
+		String line = "";
+		String cvsSplitBy = ",";
+
+		try {
+			br = new BufferedReader(new InputStreamReader(activity.getAssets().open(csvFile)));
+			while ((line = br.readLine()) != null) {
+				String[] result = line.split(cvsSplitBy);
+				Rank rank = new Rank();
+				rank.setDate(result[0].replace("\"",""));
+				rank.setTournament(result[1].replace("\"",""));
+				rank.setRound(result[2].replace("\"",""));
+				rank.setPoints(result[3].replace("\"",""));
+				ranks.add(rank);
+			}
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (br != null) {
+				try {
+					br.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		return ranks;
+	}
 }
